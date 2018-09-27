f6a939ab8b30612061d4386f88b550f315ecd1e1
PetersSharp <cwdwshare@gmail@.com>
2018-09-27 07:06:17 +0400

    auto commit

--- Metro/Properties/AssemblyInfo.cs
+++ Metro/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@
 #if(SVNCHECK)
 #   if(!DEBUG)    
     #error SVN - Working copy has uncommitted modifications, please commit all modifications before creating a release build. 
-    #error SVN - Working copy has multiple revisions, please update to the latest revision before creating a release build. 
+     
 #   endif
 #endif
 
@@ -52,6 +52,6 @@ using SpchListBuilder.Properties;
     ResourceDictionaryLocation.None,
     ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("1.0.85.*")]
-[assembly: AssemblyFileVersion("1.0.85.0")]
-[assembly: AssemblyInformationalVersion("Build date: 2018-09-27 05:55:49; Revision date: 2018-09-27 05:35:15; Revision(s) in working copy: 79:85; WARNING working copy had uncommitted modifications.")]
+[assembly: AssemblyVersion("1.0.91.*")]
+[assembly: AssemblyFileVersion("1.0.91.0")]
+[assembly: AssemblyInformationalVersion("Build date: 2018-09-27 07:19:55; Revision date: 2018-09-27 06:02:59; Revision(s) in working copy: 95; WARNING working copy had uncommitted modifications.")]
